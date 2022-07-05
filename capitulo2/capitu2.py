46a9f151e56773eed3bccccdd28c04bd35697641
Pedro Magossi Santos <pedroms.pms@gmail.com>
2022-07-04 21:18:33 -0300

    :seedling: dealing with father

--- capitulo2/capitu2.py
+++ capitulo2/capitu2.py
@@ -4,5 +4,5 @@ from bs4 import BeautifulSoup
 html = urlopen('https://pythonscraping.com/pages/page3.html')
 bs = BeautifulSoup(html, 'html.parser')
 
-for child in bs.find('table',{'id':'giftList'}).children:
-	print(child)
+for sibling in bs.find('table',{'id':'giftList'}).tr.next_siblings:
+	print(sibling)
